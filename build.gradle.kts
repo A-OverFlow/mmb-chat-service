f13740945686cf243134218cae8e301d3d03d945
myeongseob91 <myeongseob91@naver.com>
2025-04-28 21:22:45 +0900

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+	id("org.springframework.boot") version "3.4.5"
+	id("io.spring.dependency-management") version "1.1.7"
+	kotlin("jvm") version "1.9.22"
+	kotlin("plugin.spring") version "1.9.22"
+	id("org.jetbrains.kotlin.plugin.jpa") version "1.9.22"
+}
+
+group = "com.mumulbo"
+version = "0.0.1-SNAPSHOT"
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	compileOnly("org.projectlombok:lombok")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	implementation(kotlin("stdlib-jdk8"))
+	implementation("mysql:mysql-connector-java:8.0.33")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+tasks.bootJar {
+	archiveFileName.set("ms-msa-playground.jar")
+}
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+	kotlinOptions {
+		jvmTarget = "17"
+	}
+}
+kotlin {
+	jvmToolchain(17)
+}
