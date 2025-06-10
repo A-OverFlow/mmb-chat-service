a2edc2a38d553f577b1650b68b3931a999678b6d
myeongseob91 <myeongseob91@naver.com>
2025-06-10 19:30:56 +0900

    Feat: "채팅서비스<->멤버서비스 통신"
    멤버서비스 정보를 받아와 nickname , email세팅
    Resolves: N/A
    Ref: N/A
    Related to: N/A

--- src/main/kotlin/com/mumulbo/msaplayground/MmbMsMsaPlaygroundApplication.kt
+++ src/main/kotlin/com/mumulbo/msaplayground/MmbMsMsaPlaygroundApplication.kt
@@ -2,8 +2,10 @@ package com.mumulbo.msaplayground
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = ["com.mumulbo.msaplayground.service"])
 open class MmbMsMsaPlaygroundApplication
 
 fun main(args: Array<String>) {
